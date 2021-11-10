249c587e2a5aed29cee287e185b125bfcbeea623
2496289471 <2496289471@qq.com>
2021-11-10 10:27:26 +0800

    first commit

---
+++ learngit/Shared/learngitApp.swift
@@ -0,0 +1,17 @@
+//
+//  learngitApp.swift
+//  Shared
+//
+//  Created by wangyuanying on 2021/11/10.
+//
+
+import SwiftUI
+
+@main
+struct learngitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
